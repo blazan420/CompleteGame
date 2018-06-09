e247f9436184cbef78a2b50fde4fcf641912da9a
kevin <kevinjobrien420@gmail.com>
2018-06-09 01:30:05 -0500

    #1165 App Number 11 - Part2 - Create a complete game by using Control Statements //Coding

--- app/src/main/java/com/example/kjobrien/completegame/MainActivity.java
+++ app/src/main/java/com/example/kjobrien/completegame/MainActivity.java
@@ -3,7 +3,19 @@ package com.example.kjobrien.completegame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.security.SecureRandom;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final SecureRandom secureRandomNumbers = new SecureRandom();
+
+    private enum Status { NOTSTARTEDYET, PROCEED, WON, LOST }; //Integer Constants
+
+
+    private static final int TIGER_CLAWS = 2;
+    private static final int TREE = 3;
+    private static final int CEVEN = 7;
+    private static final int LEVEN = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
