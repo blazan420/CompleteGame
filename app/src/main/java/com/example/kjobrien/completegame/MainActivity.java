3acfc8a3e2a4c11f1445ee4629f35a93e62e6ce8
kevin <kevinjobrien420@gmail.com>
2018-06-09 02:10:37 -0500

    #164 App Number 11 - Part1 - Create a complete game by using Control Statements //Coding

--- app/src/main/java/com/example/kjobrien/completegame/MainActivity.java
+++ app/src/main/java/com/example/kjobrien/completegame/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kjobrien.completegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.security.SecureRandom;
 
@@ -15,11 +18,24 @@ public class MainActivity extends AppCompatActivity {
     private static final int TIGER_CLAWS = 2;
     private static final int TREE = 3;
     private static final int CEVEN = 7;
-    private static final int LEVEN = 11;
+    private static final int WE_LEVEN = 11;
+    private static final int PANTHER = 12;
+
+    TextView txtCalculations;
+    ImageView imgDice;
+    Button btnRestartGame;
+    String oldTxtCalculationsValue = "";
+    boolean firstTime = true;
+    Status gameStatus = Status.NOTSTARTEDYET;
+    int points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCalculations = (TextView) findViewById(R.id.txtCalculations);
+
+
     }
 }
